02fb3c14c077c5abc7f558bb705dcc5124ba4774
kikeg <kikeg@kikeg.com>
2025-10-31 18:42:48 +0100

    Initial implementation of the DenseInternStore.
    This is a spin-off from https://github.com/xkikeg/okane.
    
    I'll soon add straightforward interner.

---
+++ src/dense.rs
@@ -0,0 +1,246 @@
+//! Defines intern utility providing dense packed interning.
+//!
+//! Here is the basic usage of the [`DenseInternStore`].
+//!
+//! ```
+//! use bumpalo_intern::dense::{Interned,Keyed,InternTag,DenseInternStore};
+//!
+//! // You should have a type (either struct or enum) with a unique str key.
+//! #[derive(Debug, PartialEq, Eq, Clone, Copy)]
+//! struct MyInterned<'a>(&'a str);
+//!
+//! impl<'a> Keyed<'a> for MyInterned<'a> {
+//!     fn intern_key(&self) -> &'a str {
+//!         self.0
+//!     }
+//! }
+//!
+//! impl<'a> Interned<'a> for MyInterned<'a> {
+//!     type View<'b> = MyInterned<'b>;
+//!
+//!     fn intern_from<'b>(arena: &'a bumpalo::Bump, value: Self::View<'b>) -> (&'a str, Self) {
+//!         let key = arena.alloc_str(value.0);
+//!         (key, MyInterned(key))
+//!     }
+//! }
+//!
+//! let arena = bumpalo::Bump::new();
+//! let mut interner = DenseInternStore::new(&arena);
+//!
+//! let tag: InternTag<MyInterned<'_>> = interner.ensure(MyInterned(&"foo".to_string()));
+//!
+//! assert!(matches!(interner.get(tag), Some(MyInterned("foo"))));
+//! ```
+
+use std::collections::HashMap;
+use std::fmt::Display;
+use std::iter::FusedIterator;
+use std::marker::PhantomData;
+
+use bumpalo::Bump;
+use derive_where::derive_where;
+
+/// Error on InternStore operations.
+#[derive_where(Debug, PartialEq, Eq)]
+pub struct OccupiedError<T> {
+    pub existing: InternTag<T>,
+}
+
+impl<T> Display for OccupiedError<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "given alias already exists with index {}",
+            self.existing.as_index(),
+        )
+    }
+}
+
+impl<T> std::error::Error for OccupiedError<T> {}
+
+/// Interned object tag, which is actually the number densely counts up.
+/// You need [`DenseInternStore`] to resolve the actual value.
+#[derive_where(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+// #[derive(PartialEq, Eq, Hash, Clone, Copy)]
+pub struct InternTag<T> {
+    id: usize,
+    phantom: PhantomData<T>,
+}
+
+// impl<T> Debug for InternTag<T> {
+//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//         f.debug_struct("InternTag")
+//             .field("id", self.id)
+//             .field("phantom", self.phantom)
+//             .finish()
+//     }
+// }
+
+impl<T> InternTag<T> {
+    /// Constructs InternTag out of the plain index.
+    pub fn new(id: usize) -> Self {
+        Self {
+            id,
+            phantom: PhantomData,
+        }
+    }
+
+    /// Returns the underlying usize ID.
+    pub fn as_index(&self) -> usize {
+        self.id
+    }
+}
+
+/// Value associated with the unique string key.
+pub trait Keyed<'a> {
+    /// Key of the object, used for interning.
+    fn intern_key(&self) -> &'a str;
+}
+
+/// Object retrieved from [`DenseInternStore`], which has unique string key
+/// so that we can intern its instance.
+pub trait Interned<'a>: Keyed<'a> {
+    /// Original value type, usually the same as `Self` itself.
+    type View<'b>: Keyed<'b>;
+
+    /// Interns the `self` with the given `arena`.
+    fn intern_from<'b>(arena: &'a Bump, value: Self::View<'b>) -> (&'a str, Self);
+}
+
+/// Stores interned value.
+pub struct DenseInternStore<'arena, T> {
+    items: Vec<T>,
+    refs: HashMap<&'arena str, usize>,
+    arena: &'arena Bump,
+}
+
+impl<'arena, T> DenseInternStore<'arena, T> {
+    /// Creates a new instance.
+    pub fn new(arena: &'arena Bump) -> Self {
+        Self {
+            items: Vec::new(),
+            refs: HashMap::new(),
+            arena,
+        }
+    }
+
+    /// Returns `true` if there are no interned objects stored.
+    pub fn is_empty(&self) -> bool {
+        self.items.is_empty()
+    }
+
+    /// Returns the total number of registered interned objects.
+    /// Note it doesn't contain aliases.
+    pub fn len(&self) -> usize {
+        self.items.len()
+    }
+}
+
+impl<'arena, T> DenseInternStore<'arena, T>
+where
+    T: Copy,
+{
+    /// Returns the corresponding instance for the tag,
+    /// or `None` if it doesn't exists.
+    pub fn get(&self, tag: InternTag<T>) -> Option<T> {
+        self.items.get(tag.id).copied()
+    }
+}
+
+impl<'arena, T> DenseInternStore<'arena, T>
+where
+    T: Interned<'arena>,
+{
+    /// Interns given value and returns the corresponding tag.
+    /// Note if `value` is registered as alias, it'll return the canonical one.
+    /// If `value` is not registered yet, register the value as canonical value.
+    pub fn ensure<'a>(&mut self, value: T::View<'a>) -> InternTag<T> {
+        match self.resolve(value.intern_key()) {
+            Some(found) => found,
+            None => self.insert_impl(value),
+        }
+    }
+
+    /// Interns the given value and returns the tag, only when there's no interned object yet.
+    /// Returns `None` if insert failed because of existing element.
+    pub fn try_insert<'a>(&mut self, value: T::View<'a>) -> Result<InternTag<T>, OccupiedError<T>> {
+        if let Some(existing) = self.resolve(value.intern_key()) {
+            return Err(OccupiedError { existing });
+        }
+        Ok(self.insert_impl(value))
+    }
+
+    /// Inserts the given value with interning it.
+    /// This method doesn't check the duplication, so caller must ensure there's no dups.
+    fn insert_impl<'a>(&mut self, value: T::View<'a>) -> InternTag<T> {
+        let id = self.items.len();
+        let (key, interned) = T::intern_from(self.arena, value);
+        self.items.push(interned);
+        self.refs.insert(key, id);
+        InternTag {
+            id,
+            phantom: PhantomData,
+        }
+    }
+
+    /// Inserts given `value` as always alias of `canonical`.
+    /// Returns error if given `value` is already registered.
+    pub fn insert_alias<'a>(
+        &mut self,
+        value: T::View<'a>,
+        canonical: InternTag<T>,
+    ) -> Result<(), OccupiedError<T>> {
+        match self.resolve(value.intern_key()) {
+            Some(existing) => Err(OccupiedError { existing }),
+            None => {
+                let (interned_alias, _) = T::intern_from(self.arena, value);
+                self.refs.insert(interned_alias, canonical.id);
+                Ok(())
+            }
+        }
+    }
+}
+
+impl<'arena, T> DenseInternStore<'arena, T> {
+    /// Returns the specified value key if found, otherwise None.
+    #[inline]
+    pub fn resolve(&self, value: &str) -> Option<InternTag<T>> {
+        let i = self.refs.get(value)?;
+        Some(InternTag {
+            id: *i,
+            phantom: PhantomData,
+        })
+    }
+
+    /// Returns the Iterator for all elements.
+    pub fn iter<'container>(&'container self) -> Iter<T>
+    where
+        'arena: 'container,
+    {
+        Iter {
+            base: 0..self.items.len(),
+            phantom: PhantomData,
+        }
+    }
+}
+
+/// An iterator over the items of a [`DenseInternStore`].
+/// Compared to the underlying HashSet iterator,
+/// this struct ensures the `T` type.
+pub struct Iter<T> {
+    base: std::ops::Range<usize>,
+    phantom: PhantomData<T>,
+}
+
+impl<T> Iterator for Iter<T> {
+    type Item = InternTag<T>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.base.next().map(|id| InternTag {
+            id,
+            phantom: PhantomData,
+        })
+    }
+}
+
+impl<T> FusedIterator for Iter<T> {}
---
+++ tests/test.rs
@@ -0,0 +1,68 @@
+use bumpalo_intern::dense::{DenseInternStore, InternTag, Interned, Keyed, OccupiedError};
+
+use bumpalo::Bump;
+use pretty_assertions::assert_eq;
+
+#[derive(Clone, Copy)]
+struct MyInterned<'a>(&'a str);
+
+impl<'a> Keyed<'a> for MyInterned<'a> {
+    fn intern_key(&self) -> &'a str {
+        self.0
+    }
+}
+
+impl<'a> Interned<'a> for MyInterned<'a> {
+    type View<'b> = MyInterned<'b>;
+
+    fn intern_from<'b>(arena: &'a Bump, value: Self::View<'b>) -> (&'a str, Self) {
+        let key = arena.alloc_str(value.0);
+        (key, MyInterned(key))
+    }
+}
+
+#[test]
+fn ensure_and_resolve() {
+    let bump = Bump::new();
+    let mut store: DenseInternStore<MyInterned<'_>> = DenseInternStore::new(&bump);
+
+    let tag = store.ensure(MyInterned(&"foo".to_string()));
+    let tag2 = store.ensure(MyInterned(&"bar".to_string()));
+
+    assert_eq!(0, tag.as_index());
+    assert_eq!(InternTag::new(1), tag2);
+
+    assert_eq!("foo", store.get(tag).unwrap().0);
+    assert_eq!("bar", store.get(tag2).unwrap().0);
+}
+
+#[test]
+fn second_try_insert_fails() {
+    let bump = Bump::new();
+    let mut store: DenseInternStore<MyInterned<'_>> = DenseInternStore::new(&bump);
+
+    store.try_insert(MyInterned("unregistered")).unwrap();
+
+    let tag = store.try_insert(MyInterned("registered")).unwrap();
+
+    assert_eq!(
+        OccupiedError { existing: tag },
+        store.try_insert(MyInterned("registered")).unwrap_err()
+    );
+}
+
+#[test]
+fn insert_alias() {
+    let arena = Bump::new();
+    let mut store: DenseInternStore<MyInterned<'_>> = DenseInternStore::new(&arena);
+
+    let tag = store.try_insert(MyInterned("foo")).unwrap();
+    store
+        .insert_alias(MyInterned("foo_alias"), tag)
+        .expect("foo_alias must be registered");
+
+    assert_eq!(tag, store.ensure(MyInterned("foo")));
+    assert_eq!(tag, store.ensure(MyInterned("foo_alias")));
+
+    assert_eq!("foo", store.get(tag).unwrap().0);
+}
